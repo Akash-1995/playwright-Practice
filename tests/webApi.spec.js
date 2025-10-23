1b3962545164ec47ba6a99676a903407a82985ae
Akash Bachute <akashbachute353@gmail.com>
2025-10-23 14:59:17 +0530

    practice: api,session

---
+++ tests/webApi.spec.js
@@ -0,0 +1,47 @@
+const { test, expect, request } = require('@playwright/test');
+
+const loginPayload = {
+  userEmail: "test13@yopmail.com",
+  userPassword: "Test@123"
+};
+
+let apiContext;
+let token;
+
+test.describe('E-commerce API & UI tests', () => {
+
+  // -------------------- Login before all tests --------------------
+  test.beforeAll(async () => {
+    // Create API context
+    apiContext = await request.newContext();
+
+    // Login via API
+    const apiResponse = await apiContext.post(
+      'https://rahulshettyacademy.com/api/ecom/auth/login',
+      { data: loginPayload }
+    );
+
+    // Validate status code
+    expect(apiResponse.status()).toBe(200);
+
+    // Extract token
+    const jsonResponse = await apiResponse.json();
+    console.log('Login response:', jsonResponse);
+    token = jsonResponse.token;
+    console.log(jsonResponse.message);
+  });
+
+  // -------------------- Set token in local storage --------------------
+  test('Add token in local storage and open My Orders page', async ({ page }) => {
+    await page.addInitScript(value => {
+      window.localStorage.setItem('token', value);
+    }, token);
+
+    await page.goto('https://rahulshettyacademy.com/client/#/dashboard/myorders');
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard/myorders');
+  });
+  // webServer: {
+  //   command: 'npm run start',
+  //   port: 3000,
+  // }, 
+});
--- tests/webelements.spec.js
+++ tests/webelements.spec.js
@@ -35,3 +35,39 @@ test('Practice select dropdown in OrangeHRM', async ({ page }) => {
     // 7️⃣ Assertion: at least one user should appear
     expect(count).toBeGreaterThan(0);
 });
+
+
+
+test('@sanity Test on new page navigation', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+  console.log(`Page title: ${await page.title()}`);
+
+  await expect(page).toHaveURL(/AutomationPractice/);
+
+  const blinkingLink = page.locator('.blinkingText');
+
+  // Since it opens in the same tab, wait for navigation
+  await Promise.all([
+    page.waitForNavigation(),
+    blinkingLink.click(),
+  ]);
+
+  console.log(`New page URL: ${page.url()}`);
+  console.log(`New page title: ${await page.title()}`);
+
+  await expect(page).toHaveURL(/documents-request/);
+});
+
+
+//practice inputvalue() method
+
+test(`@abs practice `,async({page})=>{
+    await page.goto('https://sso.teachable.com/secure/9521/identity/login/otp');
+     let email= page.locator('[placeholder="you@email.com"]');
+     await email.fill('test@yopmail.com');
+
+     console.log(await email.inputValue());
+})
